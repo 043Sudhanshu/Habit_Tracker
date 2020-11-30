38ea259c32d22e2dd3b9d0454de247dd643a8c73
Sudhanshu chauhan <64828658+043Sudhanshu@users.noreply.github.com>
2020-12-01 01:03:34 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,61 +1,26 @@
+//require express
 const express=require('express');
 const app=express();
-
+//db
 const db=require('./config/mongoose');
 const HABIT=require('./model/habit');
+
+//ejs
 const path=require('path');
 app.set('view engine','ejs');
 app.set('views',path.join(__dirname,'views'));
 
+//parsing form data 
 app.use(express.urlencoded());
+//assets will contain js,css
 app.use(express.static('assets'));
 
-app.get('/',function(req,res){
-    HABIT.find({},function(err,Habit){
-        return res.render('home',{
-            Habits:Habit
-         });
-});
-    
-});
-
-app.get('/remove',function(req,res){
-     HABIT.findOne({name:req.query.name},function(err,Habit){
-       Habit.remove();
-       return res.redirect('back');
-     });
-});
-
-app.post('/addhabit',function(req,res){
-    req.body.Date_map={};
-    HABIT.create(req.body,function(err,habit){
-    });
-    return res.redirect('back');
-
-});
+//require moment to be available
 var moment = require('moment');
 app.locals.moment = require('moment');
 
-app.get('/action',function(req,res){
-      HABIT.findOne({name:req.query.name},function(err,habit){
-      let Date=req.query.Date;
-      let action=req.query.action;
-      console.log(req.query);
-      console.log(habit.Date_map.has(Date));
-        habit.Date_map.set(Date,action);
-        habit.save();
-     return res.redirect('back'); 
-    });
-});
-
-app.get('/habit',function(req,res){
-   HABIT.findOne({name:req.query.name},function(err,Habit){
-      return res.render('habit',{
-          name:Habit.name,
-          Date_map:Habit.Date_map
-      });
-   });
-});
+//all requests to routes
+app.use('/',require('./routes'));
 
 app.listen(8000,function(err){
     console.log('server is running');
